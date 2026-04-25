efe0752849a5037680f6242299194b29f38ba9c9
jgryglaszewski <janusz.gryglaszerowski@gmail.com>
2026-04-25 09:29:14 +0200

    Init sprawdza czy wyzej w katalogach nie zostal zainicjalizowany juz

--- src/main/java/glit/service/Repository.java
+++ src/main/java/glit/service/Repository.java
@@ -1,14 +1,43 @@
-package glit.service;
+// package glit.service;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Repository {
 
-    public static final String REPOSITORY_PATH = System.getProperty("user.dir");
+    private static Path REPOSITORY_PATH;
+
+    public Path getRepositoryPath() {
+        return REPOSITORY_PATH;
+    }
+
+    static Path whereIsRepo() {
+        try {
+            Path current = Paths.get(".").toRealPath();
+            while (current != null) {
+                if (Files.isDirectory(current.resolve(".glit"))) {
+                    return current;
+                }
+                current = current.getParent();
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        return null;
+    }
 
     static void init() throws IOException {
-        System.out.println(REPOSITORY_PATH);
+        REPOSITORY_PATH = whereIsRepo();
+        if (whereIsRepo() != null) {
+            System.out.println("Glit repository in " + REPOSITORY_PATH + " is already initialized.");
+            return;
+        }
+        System.out.println("Creating new repository...");
+        REPOSITORY_PATH = Path.of(System.getProperty("user.dir"));
+
         // create dirs
         File dirArray[] = {
             new File(REPOSITORY_PATH + "/.glit/objects"),
