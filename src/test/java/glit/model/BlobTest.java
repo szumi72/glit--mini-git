9c7d342ce050d95fc483990ea5138deee2537446
WSzumilas <w.szumilas72@gmail.com>
2026-04-29 22:05:39 +0200

    ObjectWriter class and test

--- src/test/java/glit/model/BlobTest.java
+++ src/test/java/glit/model/BlobTest.java
@@ -6,9 +6,14 @@ import static org.junit.jupiter.api.Assertions.*;
 public class BlobTest {
     @Test
     public void sameHashForSameContent(){
+        //Given
         Blob b1 = new Blob(("test").getBytes());
         Blob b2 = new Blob(("test").getBytes());
+
+        //When:
         assertEquals(b1.getHash(),b2.getHash());
+
+        //Then:
         assertNotEquals(b1,b2);
     }
 
